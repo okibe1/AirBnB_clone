eb6e378b5871d78d2805614764bfb95d4a2d2bd2
okibe1 <okibe4obinna@gmail.com>
2023-03-12 03:18:20 -0700

    Made first commit for air bnb project

---
+++ one/models/__init__.py
@@ -0,0 +1,9 @@
+__init__.py
+#!/usr/bin/python3
+'''Initializes the package'''
+# from models.base_model import BaseModel
+from models.engine import file_storage
+
+
+storage = file_storage.FileStorage()
+storage.reload()
---
+++ one/models/user.py
@@ -0,0 +1,16 @@
+__init__.py
+#!/usr/bin/python3
+'''This module creates a User class'''
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    '''Class for managing user objects'''
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        '''Initializes attributes for the User class'''
+        super().__init__(*args, **kwargs)
